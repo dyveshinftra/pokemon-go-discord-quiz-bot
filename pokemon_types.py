a9e1143979d1c14da26c66ac98564b1c277a0b7c
dyveshinftra <dyveshinftra@gmail.com>
2023-08-19 21:00:17 +0200

    super effective/not very effective

--- pokemon_types.py
+++ pokemon_types.py
@@ -8,8 +8,20 @@ _type_effectiveness = json.load(open('type_effectiveness.json'))
 
 
 def is_valid(pokemon_type):
-    return pokemon_type.capitalize() in _type_effectiveness
+    return pokemon_type in _type_effectiveness
 
 
 def valid_types():
     return _type_effectiveness.keys()
+
+
+def super_effective(pokemon_type):
+    return [key
+            for key, value in _type_effectiveness[pokemon_type].items()
+            if value > 1.0]
+
+
+def not_very_effective(pokemon_type):
+    return [key
+            for key, value in _type_effectiveness[pokemon_type].items()
+            if value < 1.0]
--- test_pokemon_types.py
+++ test_pokemon_types.py
@@ -4,16 +4,21 @@
 import pokemon_types
 
 
-# fire is a valid type!
-def test_fire_is_valid():
-    assert pokemon_types.is_valid('fire')
+def test_fire_is_valid_type():
+    assert pokemon_types.is_valid('Fire')
 
 
-# foo is not a valid type :-(
-def test_foo_is_invalid():
+def test_foo_is_invalid_type():
     assert not pokemon_types.is_valid('foo')
 
 
-# there are 18 Pokémon types
-def test_number_of_valid_types():
+def test_number_of_valid_types_is_eighteen():
     assert len(pokemon_types.valid_types()) == 18
+
+
+def test_fighting_is_super_effective_against_normal():
+    assert 'Normal' in pokemon_types.super_effective('Fighting')
+
+
+def test_fighting_is_not_very_effective_against_bug():
+    assert 'Bug' in pokemon_types.not_very_effective('Fighting')
