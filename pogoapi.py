77635b0f7ccea77b4be2b1cb19baf4db00d6cbb4
dyveshinftra <dyveshinftra@gmail.com>
2023-08-20 20:46:30 +0200

    very effective/normal/not very effective/ineffetive

--- pogoapi.py
+++ pogoapi.py
@@ -5,3 +5,9 @@ import json
 
 
 type_effectiveness = json.load(open('type_effectiveness.json'))
+
+# type effectiveness multiplier aliases
+SUPER_EFFECTIVE = 1.6
+NORMAL = 1.0
+NOT_VERY_EFFECTIVE = 0.625
+INEFFECTIVE = 0.390625
--- test_pogoapi.py
+++ test_pogoapi.py
@@ -2,7 +2,29 @@
 
 
 import pogoapi
+import pytest
 
 
 def test_number_of_types():
     assert len(pogoapi.type_effectiveness.keys()) == 18
+
+
+@pytest.fixture
+def fighting():
+    return pogoapi.type_effectiveness['Fighting']
+
+
+def test_fighting_is_super_effective_against_normal(fighting):
+    assert fighting['Normal'] == pogoapi.SUPER_EFFECTIVE
+
+
+def test_fighting_is_normal_against_dragon(fighting):
+    assert fighting['Dragon'] == pogoapi.NORMAL
+
+
+def test_fighting_is_not_very_effective_against_bug(fighting):
+    assert fighting['Bug'] == pogoapi.NOT_VERY_EFFECTIVE
+
+
+def test_fighting_is_ineffective_against_ghost(fighting):
+    assert fighting['Ghost'] == pogoapi.INEFFECTIVE
