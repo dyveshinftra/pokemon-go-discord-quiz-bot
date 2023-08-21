a6ebca5a5818a31140bb721dc98811bfd3cc23de
Danny Verstraeten <danny.dv.verstraeten@nokia.com>
2023-08-21 12:56:19 +0200

    first quiz item

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import env
+import item_is_super_effective_against
 
 
 intents = discord.Intents.default()
@@ -9,6 +10,10 @@ intents.message_content = True
 client = discord.Client(intents=intents, proxy=env.get('https_proxy'))
 
 
+# current quiz item
+item = None
+
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
@@ -16,13 +21,30 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global item
+
     # ignore when it's from us
     if message.author == client.user:
         return
     # only watch the quiz channel
     if message.channel.name != 'quiz':
         return
-    await message.channel.send('I saw that!')
+
+    # start the quiz
+    if not item:
+        item = item_is_super_effective_against.IsSuperEffectiveAgainst()
+        await message.channel.send(item.question())
+        return
+
+    # check answer
+    if item.is_answer_correct(message.content):
+        await message.channel.send('That is correct!')
+    else:
+        await message.channel.send(item.correct_answer())
+
+    # try next item
+    item = item_is_super_effective_against.IsSuperEffectiveAgainst()
+    await message.channel.send(item.question())
 
 
 client.run(env.get('DISCORD_TOKEN'))
